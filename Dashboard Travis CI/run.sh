c212e399e7ec313e820801f29a9c6b6a2f6f52d2
Guido Vollbach <gvollbach@databay.de>
2019-04-10 14:15:09 +0200

    reset data

--- Dashboard Travis CI/run.sh
+++ Dashboard Travis CI/run.sh
@@ -3,8 +3,22 @@ DICTO_PATH="/tmp/dicto_latest.csv"
 PHPUNIT_PATH="/tmp/phpunit_latest.csv"
 
 DATE=`date '+%Y-%m-%d-%H:%M:%S'`
-cp "$PHPUNIT_PATH" results/"phpunit_$TRAVIS_BUILD_NUMBER_$DATE.txt"
-cp "$DICTO_PATH" results/"dicto_$TRAVIS_BUILD_NUMBER_$DATE.txt"
+FOLDER_DATE=`date '+%Y-%m'`
+
+if [ ! -d "results/$FOLDER_DATE"]
+then
+	mkdir "results/$FOLDER_DATE"
+fi
+
+if [ -e "$PHPUNIT_PATH" ]
+then
+	cp "$PHPUNIT_PATH" "results/$FOLDER_DATE/phpunit_$TRAVIS_BUILD_NUMBER_$DATE.txt"
+fi
+
+if [ -e "$DICTO_PATH" ]
+then
+	cp "$DICTO_PATH" "results/$FOLDER_DATE/dicto_$TRAVIS_BUILD_NUMBER_$DATE.txt"
+fi
 
 git add . && git commit -m "$TRAVIS_BUILD_NUMBER - $DATE - $TRAVIS_EVENT_TYPE"
 git remote add results https://${VOLLNIXX_VAR}@github.com/vollnixx/vollnixx.github.io > /dev/null 2>&1
